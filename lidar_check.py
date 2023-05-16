9289f8b8ad48ce09dde2067833675e65e695f414
ken-itagaki <ken1024mimi0425@gmail.com>
2023-05-16 09:51:09 +0900

    汚い修正

--- lidar_check.py
+++ lidar_check.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import time
 
 ip_list = ["192.168.110.201",
            "192.168.110.202",
@@ -14,19 +16,22 @@ params = ["action=get&object=ethernet_all",
           "action=get&object=lidar_data&key=code_range&security_code=921223"]
 
 def ethernet_all(response,ip):
-    lidar_setting=response.json()["Body"]["IPv4"]
-    if lidar_setting == ip:
+    lidar_setting=response.json()
+    a = lidar_setting.get("Body", {}).get("Control_IP", {})
+    ipv4 = a.get("IPv4")
+    print(ipv4)
+    if ipv4 == ip:
         print(ip,"IPaddress is ok")
-    lidar_setting=response.json()["Body"]["Mask"]
-    if lidar_setting == "255.255.255.0":
+    mask = a.get("Mask")
+    if mask == "255.255.255.0":
         print(ip,"IPmask is ok")
-    lidar_setting=response.json()["Body"]["Gateway"]
+    gateway = a.get("Gateway")
     if ip == "192.168.110.201"or"192.168.110.202"or"192.168.110.204":
-        if lidar_setting == "192.168.110.1":
+        if gateway == "192.168.110.1":
             print(ip,"IPGateway is ok")
     elif ip == "192.168.120.203"or"192.168.120.211"or"192.168.120.212"or\
                "192.168.120.213"or"192.168.120.214":
-        if lidar_setting == "192.168.120.1":
+        if gateway == "192.168.120.1":
             print(ip,"IPGateway is ok")
             
 def lidar_config(response,ip):
@@ -64,22 +69,30 @@ def lidar_config(response,ip):
     lidar_setting=response.json()["Body"]["ClockSource"]
     if lidar_setting == "1":
         print(ip,"ClockSource is ok")
-    lidar_setting=response.json()["Body"]["PTPConfig"]
-    ptpconfig = lidar_setting["Domain"]
-    if ptpconfig == "0":
+    lidar_setting=json.loads(response.text)
+    ptpconfig = json.loads(lidar_setting['Body']['PTPConfig'])['Domain']
+    print(ptpconfig)
+    if ptpconfig == count:
         print(ip,"Domain is ok")
-    lidar_setting=response.json()["Body"]["PTPConfig"]
-    ptpconfig = lidar_setting["LogAnnounceInterval"]
+    lidar_setting=json.loads(response.text)
+    ptpconfig = json.loads(lidar_setting['Body']['PTPConfig'])['LogAnnounceInterval']
+    print(ptpconfig)
     if ptpconfig == "1":
         print(ip,"LogAnnounceInterval is ok")
-    lidar_setting=response.json()["Body"]["PTPConfig"]
-    ptpconfig = lidar_setting["LogSyncInterval"]
+    lidar_setting=json.loads(response.text)
+    ptpconfig = json.loads(lidar_setting['Body']['PTPConfig'])['LogSyncInterval']
+    print(ptpconfig)
     if ptpconfig == "1":
         print(ip,"LogSyncInterval is ok")
-    lidar_setting=response.json()["Body"]["PTPConfig"]
-    ptpconfig = lidar_setting["LogMinDelayReqInterval"]
+    lidar_setting=json.loads(response.text)
+    ptpconfig = json.loads(lidar_setting['Body']['PTPConfig'])['LogMinDelayReqInterval']
+    print(ptpconfig)
     if ptpconfig == "0":
         print(ip,"LogMinDelayReqInterval is ok")
+    lidar_setting=response.json()
+    a = lidar_setting.get("Body", {}).get("NoiseFiltering", {})
+    ipv4 = a.get("IPv4")
+    print(ipv4)
     lidar_setting=response.json()["Body"]["NoiseFiltering"]
     if lidar_setting == "1":
         print(ip,"NoiseFiltering is ok")
@@ -101,7 +114,7 @@ def lidar_sync(response,ip):
 def lidar_range(response,ip):
     lidar_setting=response.json()["Body"]["angle_setting_method"]
     print(lidar_setting)
-    if lidar_setting[0] == "0":
+    if lidar_setting == "0":
         print(ip,"angle_setting_methods is ok")
     lidar_setting=response.json()["Body"]["lidar_range"]
     print(lidar_setting[1])
@@ -141,8 +154,10 @@ for ip in ip_list:
     for param in params:
         try:
             repo_url = f"http://{ip}/pandar.cgi?{param}"
+            time.sleep(1)
             response = requests.get(repo_url)
             if param == params[0]:
+                print("eth")
                 ethernet_all(response,ip)
             elif param == params[1]:
                 lidar_config(response,ip)
